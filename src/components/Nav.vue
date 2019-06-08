c53f09f3221c5fd89e2420a584f7ec0757cd4140
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-06-08 22:25:48 +0530

    :sparkles: Added navbar

---
+++ src/components/Nav.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav>
+    <header>
+      <a className="nav-a" href="/">
+        <img v-bind:src="back" class="back" alt="logo"/>
+      </a>
+      <a class="home" href="https://www.adithyabhat.com/">Home</a>
+    </header>
+  </nav>
+</template>
+
+<script>
+    import back from '../assets/Arrow.png'
+
+    export default {
+        name: 'Nav',
+        data() {
+          return {
+            back
+          }
+        }
+    }
+
+</script>
+
+<style>
+    nav > header {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 20px;
+    }
+    .back {
+      width: 40px !important;
+    }
+    .home {
+      color: #444;
+      margin-top: 5px;
+    }
+</style>
