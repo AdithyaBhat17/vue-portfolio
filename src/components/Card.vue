0f1d93b87e75441ec303f90e556e5a80fa70eaf8
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-07-27 21:12:21 +0530

    :sparkles: completed project cards

---
+++ src/components/Card.vue
@@ -0,0 +1,46 @@
+<template>
+    <a target="_blank" rel="noopener noreferrer" :href="projectInfo.link">
+        <div class="thumbnail">
+            <img v-bind:src="projectInfo.image" :alt="projectInfo.name">
+            <h2 class="name">{{ projectInfo.name }}</h2>
+            <p class="desc">{{ projectInfo.description }}</p>
+            <div class="tech-stack">
+                <span :key="index" v-for="(tech, index) in projectInfo.tech">{{ tech }} </span>
+            </div>
+        </div>
+    </a>
+</template>
+
+<script>
+export default {
+    name: 'Card',
+    props: ['project'],
+    data() {
+        return {
+            projectInfo: this.project
+        }
+    }
+}
+</script>
+
+<style scoped>
+    a {
+        display: block;
+        text-decoration: none;
+        text-align: center;
+        color: #444;
+        margin-top: 55px;
+    }
+    h2 {
+        font-weight: bold;
+    }
+    span {
+        background-color: #ececec;
+        padding: 5px 8px;
+        margin: 0 2px;
+        border-radius: 4px;
+        box-shadow: 5px 5px 25px 0 rgba(0, 0, 0, 0.1);
+    }
+</style>
+
+
